ba63f107641e2abc67bf53aaa3d15ea032175fe8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-24 17:00:10 +0300

    add comments to nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,12 +1,14 @@
 pub fn nth(n: u32) -> u32 {
     let mut primes = vec![2, 3, 5];
+
     if n < 3 {
         return primes[n as usize];
     }
 
     let max = u32::MAX;
-    let mut count = 2;
+    let mut count = 2; // 0-indexed
 
+    // All even numbers are divisible by 2, so we don't need to calculate them
     for i in (7..max).step_by(2) {
         let mut is_prime = true;
         for prime in &primes {
