3351a27e252627024959d3e9f6258609ca31e97a
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-24 10:31:55 +0300

    add difference of squares fold technique

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -9,3 +9,15 @@ pub fn sum_of_squares(n: u32) -> u32 {
 pub fn difference(n: u32) -> u32 {
     square_of_sum(n) - sum_of_squares(n)
 }
+
+pub fn square_of_sum(n: u32) -> u32 {
+    (1..=n).fold(0, |sum, num| sum + num).pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).fold(0, |sum, num| sum + num.pow(2))
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
